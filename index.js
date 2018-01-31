6c892db2a29949989e9b56437467f35577007b35
Sastry.Kasibotla <sastry.kasibotla@kony.com>
2018-01-31 19:53:58 +0530

    kony file

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const request = require('request')
 const app = express()
-var kony = require("./kony-skd.js");
+var kony = require('kony-sdk.js');
 
 app.set('port', (process.env.PORT || 5000))
 
